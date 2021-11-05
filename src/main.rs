3beb10784d69d8d785d3ce6cc7cfdbd4cc6f64f0
sagar jois <sjois91@gmail.com>
2021-11-05 17:35:37 +0530

    adding functions to play and restart the game, show main menu options

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,31 @@ impl State {
             mode: GameMode::Menu,
         }
     }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        // TODO: Fill in this stub later
+        self.mode = GameMode::End;
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+
+            }
+        }
+    }
 }
 
 impl GameState for State {
